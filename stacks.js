dda1d3f7225f1c32a0df3a928567e90773dbd831
Vesko Vujovic <veskovujovic@yahoo.com>
2015-07-19 11:07:38 +0200

    first coommit
    added first function for stack

---
+++ stacks.js
@@ -0,0 +1,14 @@
+/**
+  * Implementation of the stacks in JavaScript
+  */
+
+// Initialize stack
+function Stack(){
+    this.dataStore = [];
+    this.top       =  0;
+    this.push      = push;
+    this.peek      = peek;
+    this.pop       = pop;
+
+}
+
