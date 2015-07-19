d0d1256395bc40a6ea6fc03bdedd6d06e4a39834
Vesko Vujovic <veskovujovic@yahoo.com>
2015-07-19 11:23:40 +0200

    2 more functions
    finished to more functions

--- stacks.js
+++ stacks.js
@@ -9,6 +9,32 @@ function Stack(){
     this.push      = push;
     this.peek      = peek;
     this.pop       = pop;
-
 }
+
+/*
+ * Push new element on the top of the stack
+ * Here ++ is placed after this.top  - in that way we ensure that first we asign elemnt to that position
+ * after that we've ensured that next postion is empty and ready for new value
+ */
+function push(element){
+   this.dataStore[this.top++] = element;
+}
+
+// Removes element from the stack
+function pop(element){
+    return this.dataStore[--this.top];
+}
+
+// Get value of the top element
+function peek(){
+    if(this.dataStore[this.top-1] !== 'undefined'){
+        return this.dataStore[this.top-1];
+    }
+}
+
+
+
+
+
+
 
