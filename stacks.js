0f732bc8936772cb4e203c32473bef6e9d967c49
Vesko Vujovic <veskovujovic@yahoo.com>
2015-07-19 12:33:12 +0200

    finished stacks with palindrome function
    added palindrome function

--- stacks.js
+++ stacks.js
@@ -9,6 +9,8 @@ function Stack(){
     this.push      = push;
     this.peek      = peek;
     this.pop       = pop;
+    this.length    = length;
+    this.clear     = clear;
 }
 
 /*
@@ -30,7 +32,41 @@ function peek(){
     if(this.dataStore[this.top-1] !== 'undefined'){
         return this.dataStore[this.top-1];
     }
+    return "No elements on the stack";
 }
+
+// Length return how many elements are on the stack - return this.top
+function length(){
+    return this.top;
+}
+
+// Clears all elements from the stack
+function clear(){
+    this.top = 0
+}
+
+
+// See if a word is palindrome using stack
+function isPalindrome(word){
+    var s   =  new Stack();
+    for( var i = 0; i < word.length; ++i ){
+        s.push(word[i]);
+    }
+
+    var reverseWord = "";
+    while(s.length > 0){
+        reverseWord = s.pop();
+    }
+
+    if(word == reverseWord){
+        return true;
+    }
+    else{
+        return false
+    }
+}
+
+
 
 
 
